fe40d05b8f12dcf72e97d29e681c7bac6f99e75f
zephome@gmail.com <zeppelin78>
2016-04-08 16:08:37 +0900

    메세지 보내기 버튼 추가

--- SlackBotWeb/Default.aspx
+++ SlackBotWeb/Default.aspx
@@ -1,2 +1,17 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
 
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button ID="btn_BotSendMessage" Text="Slack Bot 메세지 보내기" runat="server" OnClick="btn_BotSendMessage_Click" />
+    </div>
+    </form>
+</body>
+</html>
--- SlackBotWeb/Default.aspx.cs
+++ SlackBotWeb/Default.aspx.cs
@@ -4,6 +4,11 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        
+    }
+
+    protected void btn_BotSendMessage_Click(object sender, EventArgs e)
+    {
         // 금일날자
         DateTime now = DateTime.Now;
 
@@ -13,9 +18,9 @@ public partial class _Default : System.Web.UI.Page
 
         slackBot.PostMessage(title: "추천점심 메뉴",
                              username: "Coresoft",
-                             fallback: now.ToShortDateString() + " Lunch Bot의 추천 메뉴",
+                             fallback: now.ToShortDateString() + " Coresoft Lunch Bot의 추천 메뉴",
                              text: "오늘의 점심 " + lunch.LunchSelect() + " 어떠세요?",
                              channel: "#test",
-                             iconurl: "아이콘 URL");
+                             iconurl: "http://210.109.97.108:10000/redmine/coreicon.png");
     }
 }
